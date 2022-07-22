d4db80a472d97fd724ae18af71e36d85a78d67a0
aalok-sathe <asathe@mit.edu>
2022-07-22 14:29:00 -0400

    flesh out the interface; add a plotting method

--- surprisal/interface.py
+++ surprisal/interface.py
@@ -4,25 +4,50 @@ from abc import ABC, abstractmethod, abstractclassmethod, abstractproperty
 import typing
 
 
-
-
-
-
 class Model(ABC):
-
     def __init__(self, model_id=None) -> None:
         super().__init__()
         self.model_id = model_id
 
-    def surprisal(text: str) -> 'Surprisal':
-        pass
-
+    @abstractmethod
+    def digest(self, textbatch: typing.Union[typing.List, str]) -> "Surprisal":
+        raise NotImplementedError
 
 
 class Surprisal(ABC):
-
-    tokens: str = None
-    surprisals: typing.Any = None
-    
     def __index__(self):
         pass
+
+    def __len__(self):
+        return len(self.surprisals)
+
+    @property
+    @abstractmethod
+    def tokens(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def surprisals(self):
+        raise NotImplementedError
+
+    def lineplot(self, f=None, a=None):
+        # import plotext as plt
+        from matplotlib import pyplot as plt
+        import numpy as np
+
+        if f is None or a is None:
+            f, a = plt.subplots()
+
+        plt.plot(
+            self.surprisals + np.random.rand(len(self)),
+            ".--",
+            label=" ".join(self.tokens),
+        )
+        plt.xticks(range(0, 1 + len(self.tokens)))  # , self.tokens)
+        plt.xlabel("tokens")
+        plt.ylabel("surprisal (natural log scale)")
+        plt.legend()
+        plt.grid()
+
+        return f, a
