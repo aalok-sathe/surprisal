8a20b9fea5ef76af57a2793ca436b841bc7ebf50
aalok-sathe <asathe@mit.edu>
2022-07-22 14:28:43 -0400

    minimal test script while developing

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-
-
+from matplotlib import pyplot as plt
 from surprisal.model import HuggingFaceModel
 
 
-m = HuggingFaceModel(model_id='bert-base-uncased')
+m = HuggingFaceModel(model_id="gpt2")
+
+
+surpgen = m.digest(
+    [
+        "The cat sat on the mat.",
+        "How likely is a spicy donkey?",
+        "How likely is a spicy clock?",
+        "How likely is a spicy dish?",
+        "How likely is a spicy computer?",
+        "How likely is a spicy burrito?",
+    ]
+)
+
+
+f, a = plt.subplots()
+
+for surp in surpgen:
+    print(surp)
+
+    surp.lineplot(f, a)
+    # break
+
+plt.show()
